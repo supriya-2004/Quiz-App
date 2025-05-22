2a12bd133e75bc44f23d06eb026a01d9c15f587a
supriya <pashinesupriya@gmail.com>
2025-05-22 13:22:47 +0530

    Timer added

--- script.js
+++ script.js
@@ -73,54 +73,73 @@ const quizData = [
   }
 ];
 
-  
-  const quiz = document.getElementById("quiz");
-  const answerElements = document.querySelectorAll(".answer");
-  const questionElement = document.getElementById("question");
-  const a_text = document.getElementById("a_text");
-  const b_text = document.getElementById("b_text");
-  const c_text = document.getElementById("c_text");
-  const d_text = document.getElementById("d_text");
-  const submitButton = document.getElementById("submit");
-  
-  let currentQuiz = 0;
-  let score = 0;
-  
-  const deselectAnswers = () => {
-    answerElements.forEach((answer) => (answer.checked = false));
-  };
-  
-  const getSelected = () => {
-    let answer;
-    answerElements.forEach((answerElement) => {
-      if (answerElement.checked) answer = answerElement.id;
-    });
-    return answer;
-  };
-  
-  const loadQuiz = () => {
-    deselectAnswers();
-    const currentQuizData = quizData[currentQuiz];
-    questionElement.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-  };
-  
-  loadQuiz();
-  
-  submitButton.addEventListener("click", () => {
-    const answer = getSelected();
-    if (answer) {
-      if (answer === quizData[currentQuiz].correct) score++;
-      currentQuiz++;
-      if (currentQuiz < quizData.length) loadQuiz();
-      else {
-        quiz.innerHTML = `
-              <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-              <button onclick="history.go(0)">Play Again</button>
-          `;
-      }
-    }
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
+const timerElement = document.getElementById("time");
+
+let currentQuiz = 0;
+let score = 0;
+let timeLeft = 10;
+let timerInterval;
+
+const deselectAnswers = () => {
+  answerElements.forEach((answer) => (answer.checked = false));
+};
+
+const getSelected = () => {
+  let answer;
+  answerElements.forEach((answerElement) => {
+    if (answerElement.checked) answer = answerElement.id;
   });
+  return answer;
+};
+
+const loadQuiz = () => {
+  clearInterval(timerInterval);
+  timeLeft = 10;
+  timerElement.innerText = timeLeft;
+
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionElement.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+
+  timerInterval = setInterval(() => {
+    timeLeft--;
+    timerElement.innerText = timeLeft;
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      autoSubmit();
+    }
+  }, 1000);
+};
+
+const autoSubmit = () => {
+  const answer = getSelected();
+  if (answer === quizData[currentQuiz].correct) score++;
+  currentQuiz++;
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+    quiz.innerHTML = `
+      <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+      <button onclick="history.go(0)">Play Again</button>
+    `;
+  }
+};
+
+submitButton.addEventListener("click", () => {
+  clearInterval(timerInterval);
+  autoSubmit();
+});
+
+loadQuiz();
